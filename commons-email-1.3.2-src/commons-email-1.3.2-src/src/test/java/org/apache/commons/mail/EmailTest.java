5f494ec3662fe01cea981dfbd9311d373dd5ac0b
Leila Fawaz <ismaill@umich.edu>
2021-04-08 19:10:46 -0400

    Add addHeader Test

--- commons-email-1.3.2-src/commons-email-1.3.2-src/src/test/java/org/apache/commons/mail/EmailTest.java
+++ commons-email-1.3.2-src/commons-email-1.3.2-src/src/test/java/org/apache/commons/mail/EmailTest.java
@@ -63,4 +63,29 @@ public class EmailTest {
 			
 			assertEquals(0, email.getCcAddresses().size());
 		}
+		
+		// addHeader tests
+		@Test
+		public void testAddHeader() throws Exception {
+			
+			email.addHeader(Test_Names[0] , testValidChars[3]);
+			
+			assertEquals(1, email.getHeaders().size());
+		}
+		
+		@Test(expected = IllegalArgumentException.class)
+		public void testAddHeaderValue() throws Exception {
+			
+			email.addHeader("" , testValidChars[3]);
+			
+			assertEquals(0, email.getHeaders().size());
+		}
+		
+		@Test(expected = IllegalArgumentException.class)
+		public void testAddHeaderName() throws Exception {
+			
+			email.addHeader(Test_Names[1], "");
+			
+			assertEquals(0, email.getHeaders().size());
+		}
 }
